6dde9a7b6d500afe6757d7580cfab254c497983f
Admin <eaak2004@hotmail.com>
2020-03-31 06:09:36 +0100

    Add project files.

---
+++ LINQTutorials/MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace LINQTutorials
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StudentsDataGridView = new System.Windows.Forms.DataGridView();
+            this.LoadDataButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StudentsDataGridView
+            // 
+            this.StudentsDataGridView.AllowUserToAddRows = false;
+            this.StudentsDataGridView.AllowUserToDeleteRows = false;
+            this.StudentsDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.StudentsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentsDataGridView.Location = new System.Drawing.Point(12, 66);
+            this.StudentsDataGridView.MultiSelect = false;
+            this.StudentsDataGridView.Name = "StudentsDataGridView";
+            this.StudentsDataGridView.ReadOnly = true;
+            this.StudentsDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.StudentsDataGridView.Size = new System.Drawing.Size(776, 372);
+            this.StudentsDataGridView.TabIndex = 0;
+            // 
+            // LoadDataButton
+            // 
+            this.LoadDataButton.Location = new System.Drawing.Point(13, 16);
+            this.LoadDataButton.Name = "LoadDataButton";
+            this.LoadDataButton.Size = new System.Drawing.Size(142, 35);
+            this.LoadDataButton.TabIndex = 1;
+            this.LoadDataButton.Text = "Load Data";
+            this.LoadDataButton.UseVisualStyleBackColor = true;
+            this.LoadDataButton.Click += new System.EventHandler(this.LoadDataButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LoadDataButton);
+            this.Controls.Add(this.StudentsDataGridView);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LINQ Test";
+            ((System.ComponentModel.ISupportInitialize)(this.StudentsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView StudentsDataGridView;
+        private System.Windows.Forms.Button LoadDataButton;
+    }
+}
+
---
+++ LINQTutorials/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using LINQTutorials.Database;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQTutorials
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadDataButton_Click(object sender, EventArgs e)
+        {
+            List<Student> students = new List<Student>();
+
+            using (LINQTestContext db = new LINQTestContext())
+            {
+                // First Get the data
+                students = db.Students.ToList();
+
+                // Then show just the column you want to show.
+                students = students.Select(x => new Student()
+                {
+                    StudentId = x.StudentId,
+                    FullName = x.FullName
+                }).ToList();
+            }
+
+            StudentsDataGridView.DataSource = students;
+        }
+    }
+}
